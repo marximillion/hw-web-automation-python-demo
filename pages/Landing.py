e58a8f098071e55203c58b787696efa84c97dcfe
Mark De Guzman <mdguzman@homewoodhealth.com>
2026-02-11 17:27:36 -0700

    Add Landing Page constants

---
+++ pages/Landing.py
@@ -0,0 +1,42 @@
+class LandingPage:
+    EN = {
+        "base_url": "https://www.homeweb.ca",
+        "elements": {
+            "resources": {
+                "emotional_intelligence": "//a[@title='Emotional Intelligence: Why it’s Important and How to Build it']",
+                "anxiety": "//a[@title='Anxiety: Strategies for Coping with Change and Uncertainty']",
+                "letting_go": "//a[@title='Letting Go of Expectations']",
+                "toolkit": "//a[@class='btn btn-lg btn-primary']",
+            },
+            "buttons": {
+                "sign_in": "//a[@title='Sign in']//i[@class='fa-solid fa-chevron-right font-size-std']"
+            }
+        },
+        "paths": {
+            "resources": {
+                "emotional_intelligence": "/homewood/neurodiversity-focus-energy-drain-masking",
+                "anxiety": "/homewood/emotional-intelligence-why-its-important-how-build",
+                "letting_go": "/homewood/ai-anxiety-strategies-coping-with-change-uncertainty",
+                "toolkit": "/homewood/navigating-uncertainty-your-mental-health-toolkit",
+            },
+            "buttons": {
+                "sign_in": "/en/login"
+            }
+        }
+    }
+
+    FR = {
+        "base_url": "https://www.homeweb.ca/fr",
+        "resources": {
+            "emotional_intelligence": "//a[@title='Intelligence émotionnelle : pourquoi elle est importante et comment la développer']",
+            "anxiety": "//a[@title='Anxiété : stratégies pour faire face au changement et à l’incertitude']",
+            "letting_go": "//a[@title='Lâcher prise des attentes']",
+            "toolkit": "//a[@class='btn btn-lg btn-primary']",
+        },
+        "paths": {
+            "emotional_intelligence": "/homewood/neurodiversity-focus-energy-drain-masking",  # adjust if different
+            "anxiety": "/homewood/emotional-intelligence-why-its-important-how-build",
+            "letting_go": "/homewood/ai-anxiety-strategies-coping-with-change-uncertainty",
+            "toolkit": "/homewood/navigating-uncertainty-your-mental-health-toolkit",
+        }
+    }
