b1fdd3bdff697da07c4a3431c5ef7e1d3c86ffd0
Mark De Guzman <mdguzman@homewoodhealth.com>
2026-02-18 11:51:56 -0700

    Add Authenticated class

---
+++ pages/Authenticated.py
@@ -0,0 +1,17 @@
+class Authenticated:
+    EN = {
+        "paths": {
+            "resources": {
+                "stop_being_busy": "/en/homewood/neurodiversity-focus-energy-drain-masking"
+            },
+        }
+    }
+
+    FR = {
+        "paths": {
+            "resources": {
+                "stop_being_busy": "/en/homewood/neurodiversity-focus-energy-drain-masking"
+            },
+        }
+    }
+
